3b963c9d44691483e7dbcbc4114c466389aae10e
Emil Martens <emil.martens@gmail.com>
2024-02-15 13:36:05 +0100

    initial commit

---
+++ api.py
@@ -0,0 +1,23 @@
+import asyncio
+import aiohttp
+from credentials import primary_key
+
+
+async def main():
+
+    headers = {
+        "Ocp-Apim-Subscription-Key": primary_key,
+        "Cache-Control": "no-cache",
+    }
+    async with aiohttp.ClientSession() as session:
+        rep = await session.get(
+            "https://apis.vinmonopolet.no/products/v0/details-normal",
+            headers=headers,
+        )
+        data = await rep.json()
+
+    print(rep)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
